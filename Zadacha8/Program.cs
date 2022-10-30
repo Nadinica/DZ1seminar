f5a25b0c1ad8df1f7417172eda3e2ee1878f9ae6
Nadin <icemylady@gmail.com>
2022-10-30 21:55:23 +0300

    Задача8,10,15

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int Even (int [] number)
+{
+    int n = 5;
+    int index = 0;
+    int even = 0;
+    while (number[index] < n)
+    {
+        int result = number[0];
+        if (number[index] % 2 == 0);
+        return result;
+    }
+    index++;
+ }
+
+int[] number = { 1, 2, 3, 4, 5 };
+
+int even = Even(number);
+Console.WriteLine(even);
+
